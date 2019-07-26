6b4e8d04dd359d99b2a6d9fa12480459792906f0
sswq2020 <a309108243@qq.com>
2019-07-26 15:07:33 +0800

    增加vuex

--- src/store/modules/commodityManage/releaseNewCommodity/index.js
+++ src/store/modules/commodityManage/releaseNewCommodity/index.js
@@ -1,10 +1,12 @@
 const store = {
     namespaced: true,
     state: {
-
+        isEdit:false   // 默认都是新增商品的页面,
     },
     mutations: {
-
+        setIsEdit(state,payload) {
+            state.isEdit = payload;
+        }
     },
     actions: {
 
