5a4c2c45992af121d3c5237d1da421b66f281972
sswq2020 <a309108243@qq.com>
2019-07-26 15:00:38 +0800

    正式环境的图片上传组件

---
+++ src/components/ImageUpload.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="avatar-uploader">
+    <el-upload
+      :action="actionUrl"
+      :show-file-list="false"
+      :on-success="handleAvatarSuccess"
+      :on-error="handleAvatarFail"
+      :before-upload="beforeAvatarUpload"
+    >
+      <i class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+  </div>
+</template>
+
+<script>
+import {hostList} from 'api/mock'
+import {ENV} from 'common/env'
+import DICT from 'util/dict'
+export default {
+  data() {
+    return {
+      fileList: [],
+      actionUrl:`${hostList[ENV]}/dfs/open/files/upload`
+    };
+  },
+  props: {
+    maxSize: {
+      type: Number,
+      default() {
+        return 3;
+      }
+    },
+    onSuccess:{
+      type: Function,
+      default: ()=>{}       
+    }
+  },
+  methods: {
+    beforeAvatarUpload(file) {
+      const maxSize = this.maxSize;
+      if (file.type.indexOf("image") === -1) {
+        this.$messageError("请上传图片文件！");
+        return false;
+      }
+      if (file.size / 1024 / 1024 > maxSize) {
+        this.$messageError(`图片请在${maxSize}MB以下！`);
+        return false;
+      }
+      return true;
+    },
+    handleAvatarSuccess(res, file, fileList) {
+      console.log(file, fileList);
+      const Code = res.code;
+      switch (Code) {
+        case DICT.SUCCESS:
+          this.onSuccess(res)
+          this.$messageSuccess("图片上传成功！");
+          break;
+        default:
+          break;
+      }
+    },
+    handleAvatarFail(err, file, fileList) {
+       this.$messageError("图片上传失败，请稍后重试！");
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.avatar-uploader {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  width: 104px;
+  height: 104px;;
+  padding: 8px;
+}
+.avatar-uploader:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 104px;
+  height: 104px;
+  line-height: 104px;
+  text-align: center;
+}
+.avatar {
+  width: 104px;
+  height: 104px;
+  display: block;
+}
+</style>
+
