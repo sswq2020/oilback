c0345708e5e9cb9cc98abda6f68731e296a014b7
sswq2020 <a309108243@qq.com>
2019-08-06 16:28:57 +0800

    图片url改为正常的

--- src/views/commodityManage/commodityForSale.vue
+++ src/views/commodityManage/commodityForSale.vue
@@ -83,17 +83,11 @@
         <template slot-scope="scope">
           <div class="goods">
             <div class="avatar">
-              <!--测试上线前改为下面的-->
-              <img v-if="listData.list[scope.$index].fileId"
+             <img v-if="listData.list[scope.$index].picUrl"
                 width="65"
                 height="64"
-                src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+                :src="listData.list[scope.$index].picUrl"
               />
-             <!-- <img v-if="listData.list[scope.$index].fileId"
-                width="65"
-                height="64"
-                :src="listData.list[scope.$index].url"
-              /> -->
               <span v-else>未设置图片</span>
             </div>
             <div class="goods-content">
