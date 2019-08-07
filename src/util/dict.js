7204eb6ef432608368b6ab0bbdbdcd9a20298f8f
sswq2020 <a309108243@qq.com>
2019-08-07 17:29:00 +0800

    新增本地字典项

--- src/util/dict.js
+++ src/util/dict.js
@@ -15,39 +15,27 @@ DICT.APP_INFO = {
     [DICT.BUSINESS_ONLINE_PLANT]:'电商平台'
 }
 
-/** 库存正常状态 */
-DICT.INVENTORY_NORMAL = '0'
-/** 库存冻结状态 */
-DICT.INVENTORY_FROZEN = '1'
-/** 库存待出库状态 */
-DICT.INVENTORY_WAITCHECKOUT = '2'
-/** 库存挂牌状态 */
-DICT.INVENTORY_HANGOUT = '3'
 
-/** 库存状态 */
-DICT.INVENTORY_STATUS = {
-    [DICT.INVENTORY_NORMAL]: "正常",
-    [DICT.INVENTORY_FROZEN]: "冻结中",
-    [DICT.INVENTORY_WAITCHECKOUT]: "待出库",
-    [DICT.INVENTORY_HANGOUT]: "挂牌中",
+/**买房会员*/
+DICT.BUYER_VIP = '2'
+/**卖房会员*/
+DICT.SELLER_VIP = '1'
+
+/**会员类型*/
+DICT.VIP_TYPE = {
+    [DICT.BUYER_VIP]: "买方会员",
+    [DICT.SELLER_VIP]: "卖方会员"
 }
 
-/**总量质押*/
-DICT.TOTAL_PLEDGE = '0'
-/**质押业务类型*/
-DICT.PLEDGE_BUSINESS_TYPE = {
-    [DICT.TOTAL_PLEDGE]: "总量质押"
+/**会员状态禁用*/
+DICT.VIP_STATUS_FROZEN = '0'
+/**会员状态正常*/
+DICT.VIP_STATUS_NORMAL = '1'
+
+/**会员状态 */
+DICT.VIP_STATUS = {
+    [DICT.VIP_STATUS_NORMAL]: "正常",
+    [DICT.VIP_STATUS_FROZEN]: "禁用",
 }
-
-/**转账过户*/
-DICT.ACCOUNT_TRANSFER = '0'
-/**过户业务类型*/
-DICT.TRANSFER_OWNERSHIP_BUSINESS_TYPE = {
-    [DICT.ACCOUNT_TRANSFER]: "转账过户"
-}
-
-
-
-
 
 export default DICT;
