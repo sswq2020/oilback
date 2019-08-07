603b4672c74cd7664a20477ff2067a4462b44503
sswq2020 <a309108243@qq.com>
2019-08-07 17:32:28 +0800

    新增会员表单模块

--- src/views/vipManage/memberForm.vue
+++ src/views/vipManage/memberForm.vue
@@ -1,30 +1,39 @@
 <template>
-    <div class="memberForm">
-
-    </div>
+  <div class="memberForm"></div>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
 export default {
-    name:"memberForm",
-    data(){
-        return {
-
-        }
-    },
-    methods:{
-
-    },
-    created(){
-
-    },
-    mounted(){
-        
+  name: "memberForm",
+  data() {
+    return {};
+  },
+  methods: {
+    GoSeller() {
+      this.$router.push({
+        path: "/web/hyw/member/member/pageSeller"
+      });
     }
-}
+  },
+  computed: {
+    ...mapState("memberForm", ["memberType", "isEdit", "memberId"])
+  },
+  created() {},
+  mounted() {
+    if (!this.memberType) {
+      this.GoSeller();
+      return;
+    }
+    if (this.isEdit && this.memberId) {
+      console.info("这是编辑页面");
+    } else {
+      console.info("这是新增页面");
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
 
