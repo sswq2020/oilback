d648bfb536b2c70947fd71b443f5d47aa8696b0a
sswq2020 <a309108243@qq.com>
2019-08-08 09:04:11 +0800

    表单的标题显示逻辑

--- src/views/vipManage/memberForm.vue
+++ src/views/vipManage/memberForm.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="memberForm"></div>
+  <div class="container single-page">
+    <hlBreadcrumb :data="breadTitle"></hlBreadcrumb>
+
+  </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState } from "vuex";
+import Dict from "util/dict.js";
+import hlBreadcrumb from "components/hl-breadcrumb";
 export default {
   name: "memberForm",
   data() {
-    return {};
+    return {
+      
+    };
   },
+  components: {
+    hlBreadcrumb
+  },  
   methods: {
     GoSeller() {
       this.$router.push({
@@ -17,7 +27,12 @@ export default {
     }
   },
   computed: {
-    ...mapState("memberForm", ["memberType", "isEdit", "memberId"])
+    ...mapState("memberForm", ["memberType", "isEdit", "memberId"]),
+    breadTitle() {
+      const TypeText = this.memberType === Dict.SELLER_VIP ? '卖方':'买方';
+      const EditText = this.isEdit ? '编辑':'新增';
+      return ['会员管理',`${TypeText}管理`,`${EditText}${TypeText}`]
+    }
   },
   created() {},
   mounted() {
