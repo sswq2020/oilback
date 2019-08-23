cd4950907ed8bc0b3faca01f2a5e32f1c56114ed
shiswq@hletong.com <shiswq@hletong.com>
2019-08-23 08:34:58 +0800

    fix: 🐛 userID 改为 id
    BREAKING CHANGE: N
    
    Closes: N

--- src/views/vipManage/sellerVipManage.vue
+++ src/views/vipManage/sellerVipManage.vue
@@ -171,7 +171,7 @@ export default {
     },
     toggle(item) {
       let that = this;
-      const { state, userId } = item;
+      const { state, id } = item;
       const text = state === Dict.VIP_STATUS_NORMAL ? "禁用卖家" : "激活卖家";
       that
         .$confirm(`确定${text}`, "提示", {
@@ -185,7 +185,7 @@ export default {
               state === Dict.VIP_STATUS_NORMAL
                 ? Dict.VIP_STATUS_FROZEN
                 : Dict.VIP_STATUS_NORMAL,
-            userId: userId
+              id
           });
           switch (res.code) {
             case Dict.SUCCESS:
