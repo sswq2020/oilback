768549c64c9607dee6afef80df09f1a31329d072
shiswq@hletong.com <shiswq@hletong.com>
2019-10-24 09:30:36 +0800

    ci: 🎡 打包生产ci更加清晰简单

--- appended.js
+++ appended.js
@@ -3,16 +3,12 @@ const path = require('path')
 
 const env = process.env.NODE_ENV
 console.log(`当前环境---------${env}`)
-let readerStream;
-if(env !== "production") {
-    return
-}else {
-    readerStream = fs.createReadStream('pro_index.html');
+
+if (env === "production") {
+    const readerStream = fs.createReadStream('pro_index.html');
+    const outName = path.join(__dirname, './public', 'index.html')
+    var writerStream = fs.createWriteStream(outName);
+    readerStream.pipe(writerStream);
 }
 
-const outName = path.join(__dirname,'./public','index.html')
-
-var writerStream = fs.createWriteStream(outName);
-
-readerStream.pipe(writerStream);
 
